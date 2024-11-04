c17f8e1d37fd8ed3e87c9fcbc867866f3f180cd7
ckdmstn <eunsu321@naver.com>
2024-11-04 22:46:12 +0900

    [#26] feat: StarScrap 엔티티 추가

---
+++ src/main/java/com/web/stard/domain/board/global/domain/enums/TableType.java
@@ -0,0 +1,10 @@
+package com.web.stard.domain.board.global.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TableType {
+    POST, STUDY, STUDYPOST
+}
