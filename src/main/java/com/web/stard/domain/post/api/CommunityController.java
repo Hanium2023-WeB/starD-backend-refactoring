003321a64d707a42e0b79206de71bdc141c5d090
ckdmstn <eunsu321@naver.com>
2025-01-09 16:00:56 +0900

    [#97] feat: 스웨거 에러 응답 값 post 추가

--- src/main/java/com/web/stard/domain/post/api/CommunityController.java
+++ src/main/java/com/web/stard/domain/post/api/CommunityController.java
@@ -6,6 +6,9 @@ import com.web.stard.domain.post.domain.dto.request.PostRequestDto;
 import com.web.stard.domain.post.domain.dto.response.PostResponseDto;
 import com.web.stard.domain.member.domain.entity.Member;
 import com.web.stard.global.domain.CurrentMember;
+import com.web.stard.global.exception.ApiErrorCodeExample;
+import com.web.stard.global.exception.ApiErrorCodeExamples;
+import com.web.stard.global.exception.error.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -22,6 +25,7 @@ public class CommunityController {
     private final PostService postService;
 
     @Operation(summary = "커뮤니티 게시글 등록", description = "카테고리로는 '없음', '취미', '공부', '잡담', '기타' 중 하나의 값을 전달해주세요.")
+    @ApiErrorCodeExample(ErrorCode.MEMBER_NOT_FOUND)
     @PostMapping
     public ResponseEntity<PostResponseDto.PostDto> createCommPost(@CurrentMember Member member,
                                                                   @Valid @RequestBody PostRequestDto.CreateCommPostDto requestDto) {
@@ -29,6 +33,9 @@ public class CommunityController {
     }
 
     @Operation(summary = "커뮤니티 게시글 수정", description = "카테고리로는 '없음', '취미', '공부', '잡담', '기타' 중 하나의 값을 전달해 주세요.")
+    @ApiErrorCodeExamples({
+            ErrorCode.POST_NOT_FOUND, ErrorCode.INVALID_ACCESS
+    })
     @PutMapping("/{commPostId}")
     public ResponseEntity<PostResponseDto.PostDto> updateCommPost(@CurrentMember Member member,
                                                                   @PathVariable(name = "commPostId") Long commPostId,
@@ -37,6 +44,9 @@ public class CommunityController {
     }
 
     @Operation(summary = "커뮤니티 게시글 삭제")
+    @ApiErrorCodeExamples({
+            ErrorCode.POST_NOT_FOUND, ErrorCode.INVALID_ACCESS
+    })
     @DeleteMapping("/{commPostId}")
     public ResponseEntity<Long> deleteCommPost(@CurrentMember Member member,
                                                @PathVariable(name = "commPostId") Long commPostId) {
@@ -44,6 +54,7 @@ public class CommunityController {
     }
 
     @Operation(summary = "커뮤니티 게시글 상세조회")
+    @ApiErrorCodeExample(ErrorCode.POST_NOT_FOUND)
     @GetMapping("/{commPostId}")
     public ResponseEntity<PostResponseDto.PostDto> getCommPostDetail(@CurrentMember Member member,
                                                                      @PathVariable(name = "commPostId") Long commPostId) {
